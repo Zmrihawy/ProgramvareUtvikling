b0abea54ee6db4334f6b1842a14200754b358bf5
Zmrihawy <zmrihawy@stud.ntnu.no>
2019-02-19 00:12:13 +0100

    changes

--- chefsApprentice/recipe/models.py
+++ chefsApprentice/recipe/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        blank=False,
+        max_length=100,
+    )
+
+    info = models.TextField(
+        blank=False,
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='authored_posts',
+        blank=True,
+    )
+    name = models.CharField(
+        blank=False,
+        max_length=100,
+    )
+    description = models.TextField(
+        blank=False,
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        related_name='posts',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
 # Create your models here.
