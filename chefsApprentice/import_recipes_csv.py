c6a18eb072d958189ecfa2901c026f54e14cd2ba
Nikolas Emil Martinsen <nikolas.emil@hotmail.com>
2019-03-02 13:24:40 +0100

    added field for user in .csv

--- chefsApprentice/import_recipes_csv.py
+++ chefsApprentice/import_recipes_csv.py
@@ -1,5 +1,5 @@
 import csv
-from recipe.models import Recipe
+from recipe.models import Recipe, Ingredient
 from django.contrib.auth.models import User
 CSV_PATH = 'recipes.csv'
 
@@ -11,12 +11,14 @@ with open(CSV_PATH, newline='') as csvfile:
     for row in read:
         #Recipe.objects.create(user=row[0], name=row[1], description=row[2], instruction=row[3])
         recipe = Recipe()
-        recipe.user= User.objects.get(username='admin')
+        recipe.user= User.objects.get(username=row[0])
         recipe.name=row[1]
         recipe.description=row[2]
         recipe.instruction=row[3]
-        #recipe.image= row[4]
+        recipe.ingredients= Ingredient.objects.get(Ingredient.set())
+        #recipe.image= row[5]
         #recipe.image.save('recipe_image', open(''))
         recipe.save()
         count += 1
-    print(f'{str(count)} inserted successfully! ')
+        print(f'{str(row[0])} added recipe nr. {str(count)}')
+    print(f'{str(count)} recipes inserted successfully! ')
