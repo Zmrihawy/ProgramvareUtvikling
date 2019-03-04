7e51711ac576a252241ad3c52920b99e0fa75b90
siwsmith <siw.smith@hotmail.com>
2019-03-04 14:11:36 +0100

    Fixed user login system

--- chefsApprentice/users/views.py
+++ chefsApprentice/users/views.py
@@ -8,18 +8,10 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            user1 = form.save()
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-
-            chef_group = Group.objects.create(name = 'chef')
-            user_group = Group.objects.create(name = 'user')
-            user = request.POST.get('user')
-            if user:
-                user1.groups.add(user_group)
-            else:
-                user1.groups.add(chef_group)
-        return redirect('/')
+            return redirect('/')
 
     else:
         form = UserRegisterForm()
@@ -27,4 +19,11 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
+#chef_group = Group.objects.create(name = 'chef')
+            #user_group = Group.objects.create(name = 'user')
+#if user:
+               # user1.groups.add(user_group)
+            #else:
+                #user1.groups.add(chef_group)
 
+    #user = request.POST.get('user')
