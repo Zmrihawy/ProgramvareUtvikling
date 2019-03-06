18be3c737dc8ce410e6c155de704ec9ca19b7e6e
siwsmith <siw.smith@hotmail.com>
2019-03-06 08:34:41 +0100

    Made a user login page where users kan log in after they have registered. Also created link to user profile.

--- chefsApprentice/users/views.py
+++ chefsApprentice/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -13,14 +14,17 @@ def register(request):
             for group in groups:
                 user.groups.add(group)
 
-            messages.success(request, f'Account created for {username}!')
-            return redirect('/')
+            messages.success(request, f'Your account was successfully created! You are now able to log in')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
 
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
 #chef_group = Group.objects.create(name = 'chef')
             #user_group = Group.objects.create(name = 'user')
