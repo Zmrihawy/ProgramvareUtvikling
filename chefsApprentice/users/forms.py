b267a5ccc1eefadb96e1348b17909980ee5b39c7
TorsteinOtterlei <torstein@otterlei.no>
2019-03-07 19:18:39 +0100

    New branch setup

--- chefsApprentice/users/forms.py
+++ chefsApprentice/users/forms.py
@@ -1,26 +1,3 @@
-<<<<<<< current
-from django import forms
-from django.contrib.auth.models import User, Group
-from django.contrib.auth.forms import UserCreationForm
-
-
-
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    #CHOICES = (('chef', 'Chef',), ('user', 'User',))
-    #choice_field = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
-    #favorite_fruit = forms.CharField(label='What is your favorite fruit?', widget=forms.RadioSelect(choices=FRUIT_CHOICES))
-    #group = forms.CharField(widget=forms.RadioSelect(choices=CHOICES))
-
-    #logging.warning(choice_field)
-    #my_group = Group.objects.get(name='my_group_name')
-    #my_group.user_set.add(your_user)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2', 'groups']
-
-=======
 from django import forms
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
@@ -38,5 +15,3 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'groups']
-
->>>>>>> before discard
--- chefsApprentice/users/views.py
+++ chefsApprentice/users/views.py
@@ -2,12 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
-<<<<<<< Updated upstream
-from django.contrib.auth.models import Group
-from django.contrib.auth.decorators import login_required
-=======
 
->>>>>>> Stashed changes
 
 def register(request):
     if request.method == 'POST':
@@ -18,14 +13,9 @@ def register(request):
             groups = form.cleaned_data.get('groups')
             for group in groups:
                 user.groups.add(group)
-
-<<<<<<< Updated upstream
-            messages.success(request, f'Your account was successfully created! You are now able to log in')
-            return redirect('login')
-=======
             messages.success(request, f'Account created for {username} you can now log in')
             return redirect('users:login')
->>>>>>> Stashed changes
+
 
     else:
         form = UserRegisterForm()
@@ -33,17 +23,4 @@ def register(request):
 
 @login_required
 def profile(request):
-<<<<<<< Updated upstream
     return render(request, 'users/profile.html')
-
-#chef_group = Group.objects.create(name = 'chef')
-            #user_group = Group.objects.create(name = 'user')
-#if user:
-               # user1.groups.add(user_group)
-            #else:
-                #user1.groups.add(chef_group)
-
-    #user = request.POST.get('user')
-=======
-    return render(request, 'users/profile.html')
->>>>>>> Stashed changes
