924bc4c9d6b6f8c0a7198976d29aba25501845c9
Nikolas Emil Martinsen <nikolas.emil@hotmail.com>
2019-03-08 21:52:13 +0100

    Added functionality for an admin to update all recipes and for authors to edit their own recipes

--- chefsApprentice/recipe/views.py
+++ chefsApprentice/recipe/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import CreateView, DetailView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Recipe, Ingredient
 from .forms import RecipeForm, IngredientForm
@@ -51,15 +51,33 @@ class RecipeDetailView(DetailView):
 
 
 
-#class RecipeEditView(UpdateView):
-#    model = Recipe
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    fields = ['name', 'description', 'instruction', 'ingredients', 'image']
+    template_name = 'recipe/recipe_update_form.html'
 
-#    template_name = "recipe/recipe_edit.html"
+    #def get_object(self, request, *args, **kwargs):
+     #   recipe = super(RecipeUpdateView, self).get_object(*args, **kwargs)
+      #  user = request.user
+       # if not(recipe.user == self.request.user or user.is_superuser):
+        #    raise PermissionDenied
+        #return recipe
 
-#    def get_object(self, *args, **kwargs):
-#        recipe = get_object_or_404(Recipe, pk=self.kwargs['pk'])
 
-#        return recipe.
+
+    def dispatch(self, request, *args, **kwargs):
+        form = RecipeForm(request.POST, request.FILES or None)
+        user = request.user
+        recipe = self.get_object()
+        success_url = self.get_success_url()
+            # redirects to the browsepage
+        if not (recipe.user == user or user.is_superuser):
+            raise PermissionDenied
+        return super(RecipeUpdateView,self).dispatch(request, *args, **kwargs)
+
+
+    def get_success_url(self):
+        return reverse_lazy('browse:browsepage')
 
 
 class RecipeDeleteView(LoginRequiredMixin, DeleteView):
